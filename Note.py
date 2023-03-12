e9419ca5149204b04ad424d8532f00865ec018a6
dmitriy.andreev.1995@inbox.ru <dmitriy.andreev.1995@inbox.ru>
2023-03-12 19:22:05 +0300

    устранена ошибка и копирование текста,записи в csv идут хорошо.

--- Note.py
+++ Note.py
@@ -35,7 +35,7 @@ class Note:
         note.date = str(datetime.now().strftime("%d.%m.%Y %H:%M:%S"))
 
     def to_string(note):
-        return 'id number: ' + note.id + ';' + note.title + ';' + note.body + ';' + 'дата и время:' + note.date
+        return note.id + ';' + note.title + ';' + note.body + ';' + note.date
 
     def map_note(note):
         return '\n id: ' + note.id + '\n' + 'Название: ' + note.title + '\n' + 'Описание: ' + note.body + '\n' + 'Дата публикации: ' + note.date
